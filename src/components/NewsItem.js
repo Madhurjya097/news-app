c609248a214818c4165ae2575d9ff0cbab004240
Madhurjya097 <madhurjyadas097@gmail.com>
2025-05-24 16:06:10 +0530

    Updated App

---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+const news_image = "/news_card_image.jpg"
+
+
+export default class NewsItem extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    let {title, content} = this.props
+    return (
+      <>
+      <div className="card" style={{width: "18rem"}}>
+  <img src={news_image} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h6>{title}</h6>
+    <p className="card-text">{content}</p>
+  </div>
+</div>
+      </>
+    )
+  }
+}
